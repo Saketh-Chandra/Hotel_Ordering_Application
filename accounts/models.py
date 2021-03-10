9cf6b95db0fb69ed1b738b8700aa3ee062dcacc4
Dara-Ekanth <daraekanth3@gmail.com>
2021-03-10 21:59:42 +0530

    new attributes Proof and profile_pic is added and zip_code is changed to pin_code.

--- accounts/models.py
+++ accounts/models.py
@@ -11,6 +11,7 @@ GENDER_CHOICES = [
 
 
 class CustomUser(AbstractUser):
+    name = models.CharField(max_length=50,null=True,blank=True)
     email = models.EmailField(unique=True)
     Gender = models.CharField(max_length=6, choices=GENDER_CHOICES,blank=True)
     DOB = models.DateField(max_length=8, blank=True, null=True)
@@ -18,9 +19,10 @@ class CustomUser(AbstractUser):
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits "
                                          "allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)  # validators should be a list
-
+    profile_pic = models.ImageField(default="default.png",null=True, blank=True)
+    proof = models.ImageField(blank=True,null=True)
     address1 = models.CharField(max_length=100, blank=True, null=True)
     address2 = models.CharField(max_length=100, blank=True, null=True)
-    zip_code = models.CharField(max_length=12, blank=True, null=True)
+    pin_code = models.CharField(max_length=12, blank=True, null=True)
     city = models.CharField(max_length=20, blank=True, null=True)
     country = models.CharField(max_length=12, blank=True, null=True)
