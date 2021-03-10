709f163d1d2042e83b77d567d82ed3ec6b0ee5bd
Dara-Ekanth <daraekanth3@gmail.com>
2021-03-10 22:03:42 +0530

    urls updated.

--- Hotel_Ordering_Application/urls.py
+++ Hotel_Ordering_Application/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import default_home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', default_home, name='default_home_name'),
     path('accounts/', include('accounts.url')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
