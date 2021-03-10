74ae010929f7bfc22d03b8a5d520a80cf9bb6523
Dara-Ekanth <daraekanth3@gmail.com>
2021-03-10 22:00:34 +0530

    settings url is added and name for logout is altered.

--- accounts/url.py
+++ accounts/url.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [path('hello/',views.hello_world,name='hello'),
                path('login',views.login_views,name='login_page'),
                path('register',views.register_views,name='register_page'),
-               path('logout',views.logout_view,name='logout_page')
+               path('logout',views.logout_view,name='logout'),
+               path('route/settings/',views.settings_view,name='settings'),
                ]
