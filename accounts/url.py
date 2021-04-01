7925e11929776db8fe919d5e6e906b1f7933f12c
Dara-Ekanth <daraekanth3@gmail.com>
2021-04-01 22:17:52 +0530

    update password url is added.

--- accounts/url.py
+++ accounts/url.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('hello/',views.hello_world,name='hello'),
-               path('login',views.login_views,name='login_page'),
-               path('register',views.register_views,name='register_page'),
-               path('logout',views.logout_view,name='logout'),
-               path('route/settings/',views.settings_view,name='settings'),
-               path('update/profile/picture',views.update_profile_view,name="update_profile_pic"),
+urlpatterns = [path('hello/', views.hello_world, name='hello'),
+               path('login', views.login_views, name='login_page'),
+               path('register', views.register_views, name='register_page'),
+               path('logout', views.logout_view, name='logout'),
+               path('route/settings/', views.settings_view, name='settings'),
+               path('update/profile/picture', views.update_profile_view, name="update_profile_pic"),
+               path('update/password', views.update_password.as_view(template_name="accounts/update_password.html"),
+                    name="update_password"),
                ]
